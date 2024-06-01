edff3232e4fb6d313cf15c5b0c0d819f118b763b
JohnDev <andreinicolas.codebank@gmail.com>
2024-06-01 15:36:14 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//import React from 'react';
+import Print from './components/Print';
+
+function App() {
+  return (
+    <>
+      <Print />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Print.jsx
@@ -0,0 +1,11 @@
+//import React from 'react';
+
+function Print() {
+    return (
+        <div>
+            <h1 className="text-blue-500">Hello World</h1>
+        </div>
+    );
+}
+
+export default Print;
