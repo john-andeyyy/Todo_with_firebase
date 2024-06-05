1644f2cbb67cd2b51b11e35e7339b5e3f8516aa8
JohnDev <andreinicolas.codebank@gmail.com>
2024-06-05 14:07:49 +0800

    andrei

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-
+import React from 'react';
 import Body from './components/Body';
-// document.body.style.backgroundColor = '#0E0E11';
+import Header from './components/Header'; 
+
+//   document.body.style.backgroundColor = '#0E0E11';
 
 
 function App() {
   return (
     <>
-      <div>
-        <div className="flex justify-between pt-4 px-3 w-[32rem] mx-auto">
-
-          <div className="">
-            <span className="material-symbols-outlined">
-              menu
-            </span>
-          </div>
-          <div className="">
-
-            <span className="material-symbols-outlined">
-              notifications
-            </span>
-
-
-          </div>
-        </div>
-
-      </div>
-      <Body/>
+      <Header /> 
+      <Body />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="flex justify-between pt-4 px-3 w-[32rem] mx-auto">
+            <div>
+                <span className="material-symbols-outlined">
+                    menu
+                </span>
+                
+            </div>
+            <div>
+                <span className="material-symbols-outlined">
+                    notifications
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
