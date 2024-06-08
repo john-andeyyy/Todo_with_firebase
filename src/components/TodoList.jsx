f40d6280aadd28d6fb85e1fd58f66e07a42ec746
JohnDev <andreinicolas.codebank@gmail.com>
2024-06-08 13:25:00 +0800

    a

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,9 +4,19 @@ import SingleTodo from './SingleTodo';
 export function TodoList({ todos, toggleMark, toggleUpdate }) {
     return (
         <div>
-            {todos.map(todo => (
-                <SingleTodo key={todo.id} todo={todo} toggleMark={toggleMark} toggleUpdate={toggleUpdate} />
-            ))}
+            {todos.length > 0 ? (
+                todos.map(todo => (
+                    <SingleTodo key={todo.id} todo={todo} toggleMark={toggleMark} toggleUpdate={toggleUpdate} />
+                ))
+            ) : (
+                <div className="text-white font-medium flex ">
+
+                    <h1 className='m-auto'>Nothing to show</h1>
+                </div>
+            )}
+
+
+
         </div>
     );
 }
