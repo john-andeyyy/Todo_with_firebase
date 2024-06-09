85a66ba20ad6acb48aad54b29b9092e8d48b22c5
JohnDev <andreinicolas.codebank@gmail.com>
2024-06-09 14:19:41 +0800

    a

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -30,34 +30,48 @@ export function CreateTodo({ setTodos, toggleCreateTodo, showCreateTodo }) {
         }
     };
 
+    const close = () => {
+        toggleCreateTodo()
+        setNewTitle('')
+        setNewDescription('')
+    }
+
     return (
 
-        <Modal isVisible={showCreateTodo} handleClose={() => toggleCreateTodo()}>
+        <Modal isVisible={showCreateTodo} handleClose={() => close()}>
 
             {showCreateTodo && (
                 <div className="">
 
-                    <h4 className='font-semibold text-xl'>Create Todo</h4>
-                    <p className='text-gray-400'>{count} / 40</p>
-                    <input
-                        type="text"
-                        placeholder='Task Name'
-                        value={newTitle}
-                        maxLength='40'
-                        onChange={(e) => {
-                            setNewTitle(e.target.value);
-                            setCount(40 - e.target.value.length);
-                        }}
-                        className="w-full py-3 px-3 rounded-lg border border-gray focus:outline-none shadow my-4 text-sm"
-                    />
-                    <textarea
-                        placeholder='Add Description'
-                        value={newDescription}
-                        onChange={(e) => setNewDescription(e.target.value)}
-                        className="w-full py-3 px-3 rounded-lg border border-gray focus:outline-none shadow text-sm resize-none"
-                    />
-                    <div className="bg-blue-500 text-white text-center font-semibold text-xl rounded-xl">
-                        <button className="bg-blue-500 text-white text-center font-semibold text-xl py-2 rounded-xl w-full"
+                    <h4 className='font-semibold text-xl pb-3'>Create Todo</h4>
+                    <div className="">
+
+                        <input
+                            type="text"
+                            placeholder='Task Name'
+                            value={newTitle}
+                            maxLength='40'
+                            onChange={(e) => {
+                                setNewTitle(e.target.value);
+                                setCount(40 - e.target.value.length);
+                            }}
+                            className="w-full py-3 px-3 rounded-lg border border-gray focus:outline-none shadow  text-lg"
+                        />
+                        <p className='text-gray-400 text-right'>{count} / 40</p>
+
+                    </div>
+                    <div className="py-5">
+
+                        <textarea
+                            placeholder='Add Description'
+                            value={newDescription}
+                            onChange={(e) => setNewDescription(e.target.value)}
+                            className="w-full py-3 px-3 rounded-lg border border-gray focus:outline-none shadow text-lg resize-none"
+                        />
+                    </div>
+
+                    <div className="bg-blue-500 text-white text-center font-semibold text-xl rounded-xl ">
+                        <button className="bg-blue-500 text-white text-center font-semibold text-xl py-3 rounded-xl w-full"
                             onClick={addTodo}>
                             Create
                         </button>
--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -7,7 +7,7 @@ export function Updateform({
     updateTodo, setTodos, todos
 }) {
     const [isEditVisible, setIsEditVisible] = useState(false);
-
+    const [alert, setalert] = useState()
     const [count, setCount] = useState(40);
 
     // Update the count when the updatedTitle changes
@@ -17,9 +17,18 @@ export function Updateform({
         }
     }, [updatedTitle]);
 
+
+
     const handleUpdateClick = () => {
-        updateTodo();
-    setIsEditVisible(false)
+        if (count != 40) {
+            updateTodo();
+            setIsEditVisible(false)
+            setalert()
+        } else (
+            setalert("Please Enter new Title!!!")
+        )
+
+
     };
 
 
@@ -31,36 +40,38 @@ export function Updateform({
         }}>
             {currentTodo && (
                 <div>
-                    <div className="flex justify-between py-2">
-                        <div className="flex-1">
-                            <h2 className="font-bold text-xl break-all">
-                                Title: <span className='pl-4 font-medium'>{currentTodo.title}</span>
-                            </h2>
+                    {/* //! show title and description only  */}
+
+
+
+                    <div className={isEditVisible ? 'hidden' : ''}>
+
+                        <div className="flex justify-between py-2">
+                            <div className="flex-1">
+                                <h2 className="font-bold text-lg break-all">
+                                    <p>Title:</p> <span className='pl-4 capitalize font-normal  text-xl '>{currentTodo.title}</span>
+                                </h2>
+                            </div>
+                            <div className="flex-shrink-0">
+                                <p className='text-gray-400'>{currentTodo.time}</p>
+                            </div>
                         </div>
-                        <div className="flex-shrink-0">
-                            <p className='text-gray-400'>{currentTodo.time}</p>
-                        </div>
+
+                        <p className='pb-2 font-bold text-lg'>Description: <br />
+                            <span className='text-black pl-4  break-words capitalize font-normal text-xl '>
+
+                                {/*  currentTodo.description if empty it show nothing to show */}
+                                {currentTodo.description ? currentTodo.description : 'Nothing to show'}
+                            </span></p>
                     </div>
-                    <p className='text-gray-400 pb-4'>Description: <br />
-                        <span className='text-black pl-4  break-words capitalize font-medium'>
-
-                            {/*  currentTodo.description if empty it show nothing to show */}
-                            {currentTodo.description ? currentTodo.description : 'Nothing to show'}
 
 
-                            {/* {currentTodo.description} */}
-
-
-                        </span></p>
-
-
+                    {/* //! this is the update inputs */}
                     <div id="edit" className={isEditVisible ? '' : 'hidden'}>
-                        <p className='text-gray-400'>{count} / 40</p>
-
-                        <div id="Title" className='py-4'>
+                        <h1 className='text-red-500 font-semibold py-2 text-xl text-center'>{alert}</h1>
+                        <div id="Title" className='py-2'>
                             <p className='text-gray-400'>Title :</p>
                             <input
-
                                 type="text"
                                 maxLength="40"
                                 value={updatedTitle}
@@ -68,8 +79,9 @@ export function Updateform({
                                     setUpdatedTitle(e.target.value);
                                     setCount(40 - e.target.value.length);
                                 }}
-                                className="w-full py-3 px-3 rounded-lg border border-gray focus:outline-none shadow  text-sm"
+                                className="w-full py-3 px-3 rounded-lg border border-gray focus:outline-none shadow text-lg"
                             />
+                            <p className='text-gray-400 pt-2'>{count} / 40</p>
 
                         </div>
 
@@ -77,7 +89,7 @@ export function Updateform({
                         <textarea
                             value={updatedDescription}
                             onChange={(e) => setUpdatedDescription(e.target.value)}
-                            className="w-full py-3 px-3 rounded-lg border border-gray focus:outline-none shadow text-sm resize-none"
+                            className="w-full py-3 px-3 rounded-lg border border-gray focus:outline-none shadow text-lg resize-none"
                         />
 
                         <div className="text-center bg-blue-500 text-white rounded-2xl font-bold mt-8">
@@ -86,8 +98,8 @@ export function Updateform({
                             </button>
                         </div>
 
-                        <button className=' p-5 px-5 font-semibold text-xl ' onClick={() => setIsEditVisible(!isEditVisible)}>
-                            Back
+                        <button className=' p-5 px-5 font-semibold text-xl text-red-500' onClick={() => setIsEditVisible(!isEditVisible)}>
+                            Discard
                         </button>
                     </div>
 
@@ -100,14 +112,14 @@ export function Updateform({
                                 Edit
                             </button>
                         </div>
-                        <div className={`flex  pt-1 ${isEditVisible ? 'hidden' : ''}`}>
-                            <button className='p-3 px-5 text-red-500  rounded-xl font-semibold ml-auto' onClick={() => {
+
+                        <div className={`flex  py-5 ${isEditVisible ? 'hidden' : ''}`}>
+                            <button className='p-3 px-5 text-white  rounded-xl font-semibold m-auto bg-red-500 w-full' onClick={() => {
                                 setTodos(todos.filter(todo => todo.id !== currentTodo.id));
                                 setShowUpdate(false);
                             }}>
                                 Delete
                             </button>
-
 
                         </div>
                     </div>
