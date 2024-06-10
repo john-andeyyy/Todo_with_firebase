af6b8099c1c28cd29387c8503665ce1ac5ec79a1
JohnDev <andreinicolas.codebank@gmail.com>
2024-06-11 00:12:36 +0800

    a

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -1,12 +1,17 @@
 // src/components/LoginPage.js
 import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase/firebase'; // Ensure the path is correct
+import { auth } from '../firebase/firebase';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
+
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -14,47 +19,57 @@ export default function LoginPage() {
         try {
             await signInWithEmailAndPassword(auth, email, password);
             // Redirect to another page or show a success message
-            alert('samgy');
+            alert('success');
+            navigate('/');
         } catch (error) {
             setError(error.message);
         }
     };
 
     return (
-        <div className='text-center'>
-            <h1 className="flex text-3xl font-bold p-3 rounded-xl py-4 my-5 w-[31rem] mx-auto text-white">TODAY</h1>
-            {error && <p className="text-red-500 text-center">{error}</p>}
-            <form onSubmit={handleLogin}>
-                <div className="mb-5">
-                    <input
-                        className="flex w-[31rem] py-4 px-4 rounded-lg border border-gray focus:outline-none shadow text-sm mx-auto
-                            bg-[#222630] outline-none text-white transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
-                        name="email"
-                        placeholder="Enter email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-5">
-                    <input
-                        className="flex w-[31rem] py-4 px-4 rounded-lg border border-gray focus:outline-none shadow text-sm mx-auto
-                            bg-[#222630] outline-none text-white transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
-                        name="password"
-                        placeholder="Enter password"
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="text-center text-white rounded-2xl font-bold mt-8">
-                    <button className='py-3 bg-blue-500 rounded-lg w-24' type="submit">
-                        Log in
-                    </button>
-                </div>
-            </form>
+
+        <div className='text-center flex px-2'>
+            <div className="m-auto">
+
+
+                <h1 className="flex text-3xl font-bold p-3 rounded-xl py-4 my-5 w-[31rem] mx-auto text-white">Login</h1>
+                {error && <p className="text-red-500 text-center">{error}</p>}
+                <form onSubmit={handleLogin}
+                    className='w-[31rem]'
+                >
+                    <div className="mb-5">
+                        <input
+                            className="flex w-[31rem] py-4 px-4 rounded-lg border border-gray focus:outline-none shadow text-sm mx-auto
+                        bg-[#222630] outline-none text-white transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
+                            name="email"
+                            placeholder="Enter email"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="mb-5">
+                        <input
+                            className="flex w-[31rem] py-4 px-4 rounded-lg border border-gray focus:outline-none shadow text-sm mx-auto
+                        bg-[#222630] outline-none text-white transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
+                            name="password"
+                            placeholder="Enter password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <div className="text-white rounded-2xl font-bold mt-8 text-center">
+                        <button className='py-3 bg-blue-500 rounded-lg w-28' type="submit">
+                            Log in
+                        </button>
+                    </div>
+
+                </form>
+            </div>
         </div>
     );
 }
--- src/components/Signup/SignupPage.jsx
+++ src/components/Signup/SignupPage.jsx
@@ -19,7 +19,7 @@ export default function SignUp() {
         try {
             await createUserWithEmailAndPassword(auth, email, password);
             alert('User created successfully');
-            navigate('/LoginPage'); // Navigate to login page after successful sign-up
+            navigate('/LoginPage'); 
 
         } catch (error) {
             setError(error.message);
@@ -27,37 +27,46 @@ export default function SignUp() {
     };
 
     return (
-        <div className='text-center'>
-            <h1 className='text-white font-bold text-center py-3'>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
-                <div className='text-center'>
-                    <input
-                        className='className="flex w-[31rem] py-4 px-4 rounded-lg border border-gray focus:outline-none shadow text-sm mx-auto
-                        bg-[#222630] outline-none text-white transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"'
-                        type="Email"
-                        placeholder='Enter Email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
 
-                <div className='py-3'>
-                    <input
-                        className='className="flex w-[31rem] py-4 px-4 rounded-lg border border-gray focus:outline-none shadow text-sm mx-auto
-                        bg-[#222630] outline-none text-white transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"'
-                        type="password"
-                        placeholder='Enter Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                {error && <p>{error}</p>}
-                <button type="submit"
-                className='font-semibold text-white bg-blue-500 px-10 py-2 rounded-lg hover:bg-blue-800'
-                >Sign Up</button>
-            </form>
+
+        <div className='text-center flex px-2'>
+            <div className="m-auto">
+                <h1 className='text-white font-bold text-center py-3'>Sign Up</h1>
+                <form onSubmit={handleSubmit}>
+                    
+                    
+                    <div className="mb-5">
+                        <input
+                            className="flex w-[31rem] py-4 px-4 rounded-lg border border-gray focus:outline-none shadow text-sm mx-auto
+                        bg-[#222630] outline-none text-white transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
+                            name="email"
+                            placeholder="Enter email"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="mb-5">
+                        <input
+                            className="flex w-[31rem] py-4 px-4 rounded-lg border border-gray focus:outline-none shadow text-sm mx-auto
+                        bg-[#222630] outline-none text-white transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
+                            name="password"
+                            placeholder="Enter password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+
+
+                    {error && <p>{error}</p>}
+                    <button type="submit"
+                        className='font-semibold text-white bg-blue-500 px-10 py-2 rounded-lg hover:bg-blue-800'
+                    >Sign Up</button>
+                </form>
+            </div>
         </div>
     );
 }
