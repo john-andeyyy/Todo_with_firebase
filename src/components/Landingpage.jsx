611688bb45188327cd727a2328050a37876fbbf2
JohnDev <andreinicolas.codebank@gmail.com>
2024-06-12 13:03:06 +0800

    a

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -4,7 +4,7 @@ export default function Landingpage() {
     return (
         <div className='flex justify-center items-center h-screen '>
             <div className='text-white font-bold text-center w-[31rem]'>
-                <h1>Welcome to Todo Web App...</h1>
+                <h1>Welcome to Todo Web App...x</h1>
             </div>
         </div>
     );
